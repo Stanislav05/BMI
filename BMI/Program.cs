0844c5e22b9ab27e3b2c732d4e2c3f1ff0555850
Stanislav Fučík <Fucik.Stanislav@stud.vassboskovice.cz>
2021-10-19 09:29:45 +0200

    tvoření

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -6,7 +6,33 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("BMI");
+            double váha;
+            double výška;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Výpočet BMI");
+                Console.WriteLine("-------------------------------");
+                {
+                    Console.Write("Zadej svou hmotnost (Kg)=");
+                    double.TryParse(Console.ReadLine(), out váha);
+                    if (váha <= 0)
+                    {
+                        Console.WriteLine("Pouze přirozená čísla");
+                    }
+                    double.TryParse(Console.ReadLine(), out výška);
+                    if (výška <= 0)
+                    {
+                        Console.WriteLine("Pouze přirozená čísla");
+                    }
+                    else
+                    {
+
+
+                    }
+
+                }
+            
+            }
         }
     }
-}
